2649101ed89c47f19e3e4033281af2db3dfc86ed
Lnahqis <165541382+Lane712@users.noreply.github.com>
2025-02-27 14:15:35 +0800

    Fix some error

--- requests/douban_movie_spider.py
+++ requests/douban_movie_spider.py
@@ -2,14 +2,11 @@ import os,sys,time,json,re
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0"
-}
+from tqdm import tqdm
 
 # 编译正则表达式
-PATTERN_ORIGIN = re.compile(r'<span class="pl">制片国家/地区:</span>(.*?)<br/>')
-PATTERN_LANGUAGE = re.compile(r'<span class="pl">语言:</span>(.*?)<br/>')
+PATTERN_ORIGIN = re.compile(r'<span class="pl">[\s]+制片国家/地区:[\s]+</span>\n(.*?)\n')
+PATTERN_LANGUAGE = re.compile(r'<span class="pl">[\s]+语言:[\s]+</span>\n(.*?)\n')
 
 def fetch_page(url):
     """抓取网页内容"""
@@ -23,7 +20,6 @@ def fetch_page(url):
 
 def prettify_html(html, title=None):
     """格式化并保存HTML文件"""
-    html = html.replace("<br />","") # 删除特定标签
     soup = BeautifulSoup(html, "lxml")
     if not title:
         title = re.sub(r"\s+","", soup.find("title").string)
@@ -62,9 +58,10 @@ def get_hrefs_from_html():
             json.dump(hrefs, f, ensure_ascii=False, indent=2)
     except (MemoryError, json.JSONDecodeError) as e:
         print(f"JSON write error:{e}")
+
     return hrefs
 
-def read_hrefs_from_json(filename="hrefs.json"):
+def read_hrefs_from_json(filename="豆瓣电影Top250.json"):
     """从 JSON 文件读取电影链接"""
     try:
         with open(filename, "r", encoding="utf-8") as f:
@@ -74,7 +71,8 @@ def read_hrefs_from_json(filename="hrefs.json"):
 
 def parse_movie_details(html):
     """解析电影详情页"""
-    soup = BeautifulSoup(html, "html.parser")
+    html = html.replace("<br/>","") # 删除特定字符
+    soup = BeautifulSoup(html, "lxml")
 
     # 提取关键信息
     title = soup.find("title").string.replace(" (豆瓣)", "").strip()
@@ -82,15 +80,16 @@ def parse_movie_details(html):
     rating = soup.find("strong", {"property": "v:average"}).string.strip()
 
     # 提取类型
-    genres = [span.string for span in soup.find_all("span", {"property": "v:genre"})]
+    genres = []
+    for span in soup.find_all("span", {"property": "v:genre"}):
+        genres.append(re.sub(r"\s+","", span.string))
 
-    # 提取制片国家/地区
-    origin_info = PATTERN_ORIGIN.search(str(soup))
-    origin = origin_info.group(1).strip() if origin_info else "N/A"
-
-    # 提取语言
-    language_info = PATTERN_LANGUAGE.search(str(soup))
-    language = language_info.group(1).strip() if language_info else "N/A"
+    ## 信息
+    info_div = soup.find("div", attrs={"id":"info"})
+    ## 地区
+    origin = re.sub(r'<span class="pl">[\s]+制片国家/地区:[\s]+</span>[\s]+',"",PATTERN_ORIGIN.search(str(info_div)).group()).replace("\n","").replace(" ","")
+    ## 语言
+    language = re.sub(r'<span class="pl">[\s]+语言:[\s]+</span>[\s]+',"",PATTERN_LANGUAGE.search(str(info_div)).group()).replace("\n","").replace(" ","")
 
     # 提取时长
     runtime = soup.find("span", {"property": "v:runtime"})["content"].strip()
@@ -118,9 +117,12 @@ def parse_movie_details(html):
     }
 
 def update_movies_data(movies):
-    # 读取数据
-    with open("movies.json", "a+", encoding="utf-8") as f:
-        all_movies = json.load(f)
+    try:
+        # 读取数据
+        with open("movies.json", "r", encoding="utf-8") as f:
+            all_movies = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        all_movies = []
     # 合并数据
     for movie in movies:
         all_movies.append(movie)
@@ -132,49 +134,65 @@ def main():
     # 读取电影链接
     hrefs = read_hrefs_from_json()
 
+    if not os.path.exists("./movies.json"):
+        with open("./movies.json","W",encoding="utf-8") as f:
+            pass
+
     # 排除已处理的电影链接
-    with open("movies.json", "a+", encoding="utf-8") as f:
-        processed_movies = json.load(f)
-    processed_urls = {movie['url'] for movie in processed_movies}
+    try:
+        with open("movies.json", "r", encoding="utf-8") as f:
+            processed_movies = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        processed_movies = []
+    processed_urls = [movie['url'] for movie in processed_movies]
 
     # 移除已处理的链接
     fetched_hrefs = []
-    for href in hrefs[:]:  # 遍历hrefs的副本，避免在迭代时修改列表
+    for href in hrefs:
         if href['href'] not in processed_urls:
             fetched_hrefs.append(href)
-            print(f"Remove {href['href']}")
             
     # 确保存放 HTML 的文件存在
-    if os.path.isdir("./movie_htmls"):
+    if not os.path.isdir("./movie_htmls"):
         os.makedirs("./movie_htmls")
 
-    # 初始化数据存储
-    fetched_movies = []
-    # 遍历链接并解析详情页
-    for href in fetched_hrefs:
-        url = href["href"]
-        title = href["title"]
+    # 进度条显示
+    with tqdm(total=len(fetched_hrefs), desc="FETCH MOVIE") as pbar:
+        # 初始化数据存储
+        fetched_movies = []
+        # 遍历链接并解析详情页
+        for href in fetched_hrefs:
+            url = href["href"]
+            title = href["title"]
 
-        try:
-            # 获取 HTML
-            if os.path.exists(f"./movie_htmls/{title}.html"):
-                with open(f"./movie_htmls/{title}.html") as file:
-                    html = file.read()
-            else:
-                html = fetch_page(url).text
-                prettify_html(html, title)
-            # 解析 HTML
-            movie_data = parse_movie_details(html)
-            movie_data['url'] = url
-            fetched_movies.append(movie_data)
-            print(f"Processed {title}")
-            time.sleep(3) # 请求延迟
-        except KeyboardInterrupt:
-            update_movies_data(fetched_movies)
-            print(f"Quit! Add {len(fetched_movies)} movies data.")
-            sys.exit(0)
-        except Exception as e:
-            print(f"Error processing {url}: {str(e)}")
+            try:
+                # 获取 HTML
+                if os.path.exists(f"./movie_htmls/{title}.html"):
+                    with open(f"./movie_htmls/{title}.html","r",encoding="utf-8") as file:
+                        html = file.read()
+                else:
+                    time.sleep(3) # 请求延迟
+                    res = fetch_page(url)
+                    html = res.text
+                    pbar.set_postfix(f"{res.status_code}")
+                    # print(f"{res.headers}", file=sys.stderr) # 打印其他信息
+                    # pbar.refresh()  # 手动刷新进度条
+                    html = prettify_html(html, title)
+            
+                # 解析 HTML
+                movie_data = parse_movie_details(html)
+                movie_data['url'] = url
+                fetched_movies.append(movie_data)
+                pbar.update(1)
+                
+            except KeyboardInterrupt:
+                update_movies_data(fetched_movies)
+                pbar.close()
+                print(f"Quit! Add {len(fetched_movies)} movies data.")
+                sys.exit(0)
+            except Exception as e:
+                print(f"Error processing {url}: {str(e)}")
+        pbar.close()
 
     # 存储数据
     update_movies_data(fetched_movies)
